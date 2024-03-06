5ea81304e5d10afce5eaf04f93087328ca8dbe59
Alonso Crespo <92087839+alon159@users.noreply.github.com>
2024-03-06 20:24:31 +0100

    Seguir artista hecho

--- bot.py
+++ bot.py
@@ -2,13 +2,15 @@ import logging
 from warnings import filterwarnings
 from telegram.warnings import PTBUserWarning
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import (ApplicationBuilder, ContextTypes, CommandHandler, filters, 
-                          CallbackQueryHandler, ConversationHandler, MessageHandler, PicklePersistence)
+from telegram.ext import (ApplicationBuilder, PicklePersistence, ContextTypes, 
+                          ConversationHandler, CommandHandler, CallbackQueryHandler, MessageHandler,
+                          filters)
 import ticketpy
-from dotenv import load_dotenv
-load_dotenv()
 import os
+from dotenv import load_dotenv
+load_dotenv('token.env')
 
+#Logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -16,125 +18,140 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
+#Filter warnings
 filterwarnings(action="ignore", message=r".*CallbackQueryHandler", category=PTBUserWarning)
 
+#Ticketmaster API
 ticketmaster_token = os.getenv('API_TICKETMASTER_TOKEN')
 if ticketmaster_token is None:
     raise ValueError('API_TICKETMASTER_TOKEN is not set')
-tm_client = ticketpy.Api(ticketmaster_token)
+tm_client = ticketpy.ApiClient(ticketmaster_token)
 
+#Telegram API
 telegram_token = os.getenv('API_TELEGRAM_TOKEN')
 if telegram_token is None:
     raise ValueError('API_TELEGRAM_TOKEN is not set')
 
-#ESTADOS
-SELECT_OPTION, ARTIST_SEARCH, EVENT_SEARCH, FOLLOWING, START_OVER = range(5)
+#States
+START_ROUTES, END_ROUTES = range(2)
+#Callback data
+ARTIST_SEARCH, EVENT_SEARCH, FOLLOWING = range(3)
+#Artist seach states
+ARTIST_SEARCH_RESULTS = range(1)
+#Ending conversation
+FOLLOW, START_OVER, END = range(3)
 
-# Función para enviar un mensaje con botones
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.message.from_user
     logger.info("User %s started the conversation.", user.first_name)
 
     keyboard = [
-        [InlineKeyboardButton("Buscar artista", callback_data='buscar_artista'),
-         InlineKeyboardButton("Buscar evento", callback_data='buscar_evento'),
-         InlineKeyboardButton("Artistas que sigo", callback_data='followed_artists')],
+        [InlineKeyboardButton("Buscar artista", callback_data=str(ARTIST_SEARCH)),
+         InlineKeyboardButton("Buscar evento", callback_data=str(EVENT_SEARCH)),
+         InlineKeyboardButton("Artistas que sigo", callback_data=str(FOLLOWING))]
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await update.message.reply_text("Elige una opción", reply_markup=reply_markup)
-    return SELECT_OPTION
+    return START_ROUTES
 
 async def start_over(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query=update.callback_query
+    await query.answer()
     keyboard = [
-        [InlineKeyboardButton("Buscar artista", callback_data='buscar_artista'),
-         InlineKeyboardButton("Buscar evento", callback_data='buscar_evento'),
-         InlineKeyboardButton("Artistas que sigo", callback_data='followed_artists')],
+        [InlineKeyboardButton("Buscar artista", callback_data=str(ARTIST_SEARCH)),
+         InlineKeyboardButton("Buscar evento", callback_data=str(EVENT_SEARCH)),
+         InlineKeyboardButton("Artistas que sigo", callback_data=str(FOLLOWING))]
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await query.edit_message_text("Elige una opción", reply_markup=reply_markup)
-    return SELECT_OPTION
-
-# Función para manejar las respuestas de los botones
-async def option(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    if query.data == 'buscar_artista':
-        await query.message.reply_text('Por favor, introduce el nombre del artista:')
-        return ARTIST_SEARCH
-    elif query.data == 'buscar_evento':
-        await query.message.reply_text('Por favor, introduce el nombre del evento:')
-        return EVENT_SEARCH
+    return START_ROUTES
 
 async def buscar_artista(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    resp = tm_client.attractions.find(keyword=update.message.text).all()
-    if resp is not None:
-        context.user_data['artista'] = resp
-    keyboard = []
-    keyboard.append([InlineKeyboardButton("<-- Volver", callback_data="volver")])
-    for artist in resp:
-        keyboard.append([InlineKeyboardButton(artist.name, callback_data=artist.id)])
-    #    await context.bot.send_message(chat_id=update.effective_chat.id, text=artist.name)
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Selecciona un artista:", reply_markup=reply_markup)
-    return START_OVER
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Dime el artista que quieres buscar")
+    return ARTIST_SEARCH_RESULTS
 
 async def artist_button(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    artist_id = query.data
-    context.user_data['artist_id'] = artist_id
-    await query.answer()
-    return ConversationHandler.END
-
-# Buscar un evento
-async def buscar_evento(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    resp = tm_client.events.find(keyword=update.message.text).all()
+    resp = tm_client.attractions.find(keyword=update.message.text).all()
     keyboard = []
-    for event in resp:
-        keyboard.append([InlineKeyboardButton(event.name, callback_data=event.id)])
-    #    await context.bot.send_message(chat_id=update.effective_chat.id, text=artist.name)
+    keyboard.append([InlineKeyboardButton("<-- Volver", callback_data=str(START_OVER))])
+    for artist in resp:
+        keyboard.append([InlineKeyboardButton(artist.name, callback_data=str(FOLLOW)+"_"+artist.id+"_"+artist.name)])
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Selecciona un evento:", reply_markup=reply_markup)
-    return ConversationHandler.END
+    await update.message.reply_text("Selecciona un artista:", reply_markup=reply_markup)
+    return END_ROUTES
 
-async def following(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if context.user_data.get('artista'):
-        reply_text = (
-            f"Your artista? I already know the following about that: {context.user_data['artista']}"
-        )
-    await update.message.reply_text(reply_text)
-    
+async def seguir_artista(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query=update.callback_query
+    await query.answer()
+    data=query.data.split("_")
+    artist_id = data[1]
+    artist_name = data[2]
+    # Obtiene la lista de artistas seguidos, si no existe, crea una nueva lista
+    followed_artists = context.user_data.get('artist_id', [])
 
-async def cancel(update: Update, context):
-    """Cancelar la conversación"""
-    await update.message.reply_text('Conversación cancelada.')
-    return ConversationHandler.END
+    # Añade el nuevo artista a la lista
+    followed_artists.append(artist_id)
+
+    # Guarda la lista actualizada en la persistencia
+    context.user_data['artist_id'] = followed_artists
+    context.user_data['artist_name'] = artist_name
+    keyboard=[]
+    keyboard.append([InlineKeyboardButton("<-- Volver", callback_data=str(START_OVER))])
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await query.edit_message_text("Artista "+artist_name+" ("+artist_id+ ") "+" seguido", reply_markup=reply_markup)
+
+
+async def buscar_evento(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
+async def artistas_siguiendo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Recupera los datos de la persistencia
+    artist_ids = context.user_data.get('artist_id')
+
+    if not artist_ids:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="No estás siguiendo a ningún artista.")
+        return
+
+    # Busca los artistas por sus IDs y los muestra
+    for artist_id in artist_ids:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=artist_id)
+
+async def end(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
 
 def main():
-    persistence = PicklePersistence(filepath='conversationbot')
+    persistence = PicklePersistence(filepath='conversationbot.pickle')
     application = ApplicationBuilder().token(telegram_token).persistence(persistence).build()
-    
+
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            SELECT_OPTION: [CallbackQueryHandler(option)],
-            ARTIST_SEARCH: [MessageHandler(filters.TEXT & ~filters.COMMAND, buscar_artista),
-                                CallbackQueryHandler(artist_button)],
-            EVENT_SEARCH: [MessageHandler(filters.TEXT & ~filters.COMMAND, buscar_evento)],
-            FOLLOWING: [MessageHandler(filters.TEXT & ~filters.COMMAND, following)],
-            START_OVER: [CallbackQueryHandler(start_over)],
+            START_ROUTES: [
+                CallbackQueryHandler(buscar_artista, pattern="^" + str(ARTIST_SEARCH) + "$"),
+                CallbackQueryHandler(buscar_evento, pattern="^" + str(EVENT_SEARCH) + "$"),
+                CallbackQueryHandler(artistas_siguiendo, pattern="^" + str(FOLLOWING) + "$"),
+            ],
+            ARTIST_SEARCH_RESULTS: [
+                MessageHandler(filters.TEXT, artist_button),
+            ],
+            END_ROUTES: [
+                CallbackQueryHandler(seguir_artista, pattern="^" + str(FOLLOW)),
+                CallbackQueryHandler(start_over, pattern="^" + str(START_OVER) + "$"),
+                CallbackQueryHandler(end, pattern="^" + str(END) + "$"),
+            ]
         },
-        fallbacks=[CommandHandler('cancel', cancel)],
-        name='my_conversation',
-        persistent=True
+        fallbacks=[CommandHandler('start', start)]
     )
 
+    # Add ConversationHandler to application that will be used for handling updates
     application.add_handler(conv_handler)
-    
-    application.run_polling()
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == '__main__':
     main()
